c9d9340750c08c4ca2eab8a3bfd0e15459be11d2
jesus dario rodriguez <44887174+JDRodriguez80@users.noreply.github.com>
2021-04-13 02:29:04 -0500

    Create empleado.java

---
+++ empleado.java
@@ -0,0 +1,18 @@
+package mx.jdr80.nomina;
+ 
+/**
+ *
+ * @author dario
+ */
+public class empleado {
+    public static void main (String[] args){
+        //creando objeto del tipo frmempleado 
+        frmEmpleado empleado =new frmEmpleado();
+        //seteando el formulario como visible
+        empleado.setVisible(true);
+    
+    }
+    
+   
+    
+}
